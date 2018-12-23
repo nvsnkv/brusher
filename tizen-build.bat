68c668513e80059169202f1cb9752ded8a209e19
nvsnkv <nvsnkv@live.com>
2018-12-23 18:33:18 +0300

    updated package, added build sctript

---
+++ tizen-build.bat
@@ -0,0 +1,3 @@
+tizen build-web -e node_modules/*, .build/*, .build, .sign/*, .sign, webUnitTest/*, webUnitTest, .externalToolBuilders/*, .externalToolBuilders, .buildResult/*, .buildResult, .settings/*, .settings, .package/*, .package, .tproject, .project, .sdk_delta.info, .rds_delta, *.wgt, .tizen-ui-builder-tool.xml, src/*, .gitignore, package*json, tsconfig.json, webpack.config.js, .build/*, .build, .sign/*, .sign, webUnitTest/*, webUnitTest, .externalToolBuilders/*, .externalToolBuilders, .buildResult/*, .buildResult, .settings/*, .settings, .package/*, .package, .tproject, .project, .sdk_delta.info, .rds_delta, *.wgt, .tizen-ui-builder-tool.xml
+cd .buildResult
+tizen package -t wgt
